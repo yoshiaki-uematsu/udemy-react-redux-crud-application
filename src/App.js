ff4f180f897923e213106f516b2fa3f5385b753b
y-uematsu <>
2018-11-15 17:54:36 +0900

    add Welcome message Hello, World!

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1>Hello, world!</h1>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
